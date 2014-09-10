78797319f0254c77e948d1ab632af8c583a9bb97
Tylor Sherman <tylor@denimandsteel.com>
2014-09-10 14:52:31 -0700

    Set Intercom app_id to test version

--- config/initializers/intercom.rb
+++ config/initializers/intercom.rb
@@ -1,7 +1,7 @@
 IntercomRails.config do |config|
   # == Intercom app_id
   #
-  config.app_id = ENV["INTERCOM_APP_ID"] || "l2sbzelx"
+  config.app_id = ENV["INTERCOM_APP_ID"] || "swzamogk"
 
   # == Intercom secret key
   # This is required to enable secure mode, you can find it on your Intercom
