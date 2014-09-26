252203e4d2ed1084f90c37d16f09d944a3e3f495
Arian Khosravi <arian.khosravi@gmail.com>
2014-09-26 15:41:44 -0700

    done with orgs

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,30 @@ class Role < ActiveRecord::Base
   has_many :organization_user_roles
   has_many :organizations
   has_many :users
+
+  def css_link_class 
+    case name
+    when "Employee"
+      "wr-link-employee"
+    when "Founder"
+      "wr-link-founder"
+    when "Freelancer/Contractor"
+      ""
+    when "Board Member"
+      ""
+    when "Investor"
+      "wr-link-investor"
+    when "Service Provider"
+      ""
+    when "Consultant"
+      "wr-link-advisor"
+    when "Advisor"
+      "wr-link-advisor"
+    when "Client"
+      ""
+    when "User"
+      ""
+    end
+  end
+
 end
