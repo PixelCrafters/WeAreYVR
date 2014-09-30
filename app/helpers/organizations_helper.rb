1913012690c627786cdf412c915147b0d5dae106
Tylor Sherman <tylor@denimandsteel.com>
2014-09-29 17:34:24 -0700

    Tidy profiles a bit

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -6,15 +6,15 @@ module OrganizationsHelper
   def display_address
     address = ""
     if @organization.main_address.address1
-      address = "#{@organization.main_address.address1}<br />"
+      address = "#{@organization.main_address.address1}<br>"
     end
     if @organization.main_address.address2
-      address += "#{@organization.main_address.address2}<br />"
+      address += "#{@organization.main_address.address2}<br>"
     else
-      address += "#{Rails.application.secrets.location_name}<br />"
+      address += "#{Rails.application.secrets.location_name}<br>"
     end
     if @organization.main_address.zip
-      address += "#{@organization.main_address.zip}<br />"
+      address += "#{@organization.main_address.zip}<br>"
     end
     address.html_safe
   end
